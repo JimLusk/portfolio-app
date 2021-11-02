a8f5b2a5e4924868cd29a3b1c16f83e85971baac
Jim Lusk <jblusk@gmail.com>
2021-11-01 17:11:03 -0700

    Added TODO's for upcoming iterations.

--- portfolio-express-server/routes/portfolios.js
+++ portfolio-express-server/routes/portfolios.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET portfolios listing. */
+//TODO: Update this to return a default portfolio JSON listing.
+//TODO: Update this to return desired portfolio from relational database.
 router.get('/', function(req, res, next) {
-  res.send('This is the call into the portfolios route');
+  res.send('GET: /portfolios route');
 });
 
 module.exports = router;

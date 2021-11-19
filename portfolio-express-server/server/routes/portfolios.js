abce730ec88b79dda05d358bd7cbd0d9d7a17eea
J Lusk <jblusk@gmail.com>
2021-11-18 16:06:09 -0800

    Error handling for portfolios route-no portfolio.

--- portfolio-express-server/server/routes/portfolios.js
+++ portfolio-express-server/server/routes/portfolios.js
@@ -17,6 +17,14 @@ router.get('/', function(req, res, next) {
 //return portfolio for given portfolio id
 router.get('/:id', function(req, res, next) {
   console.log("Portfolio ID parameter is: " + req.params.id);
+  let portfolioId=Number(req.params.id)-1;
+  portfolioObj=portfolioData[portfolioId];
+  if (portfolioObj==null) {
+    res.status(404).send("Portfolio ID " + portfolioId + " not found.");
+  } else {
+    res.send(portfolioObj);
+  }
+  
 });
 
 

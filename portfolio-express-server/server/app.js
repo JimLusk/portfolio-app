57404a31ea6ab5aaa72f769f827fd1368a0ad1c2
Jim Lusk <jblusk@gmail.com>
2021-11-23 14:25:52 -0800

    Cleanup of garbatge/experiment code.

--- portfolio-express-server/server/app.js
+++ portfolio-express-server/server/app.js
@@ -1,10 +1,10 @@
-//import data from 'data/portfolio-data-100-records.json';
 
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');

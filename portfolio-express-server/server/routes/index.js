fa90f7b9a71dc87a3b429464f8ac3d4b61897442
Jim Lusk <jblusk@gmail.com>
2021-11-29 18:38:24 -0800

    Add database integration for portfolio get.

--- portfolio-express-server/server/routes/index.js
+++ portfolio-express-server/server/routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* Any request at service root */
+router.all('/', function(req, res, next) {
   res.status(404).send("No endpoint found");
 });
 
--- portfolio-express-server/server/routes/portfolios.js
+++ portfolio-express-server/server/routes/portfolios.js
@@ -9,9 +9,6 @@ var pgp = require('pg-promise')(/* options */)
 var db = pgp('postgres://admin:admin@localhost:5432/portfolio-db')
 
 
-//const db = require('../services/db');
-
-
 
 /* GET portfolios listing. */
 //TODO: Externalize SQL to configuration
@@ -21,7 +18,7 @@ var db = pgp('postgres://admin:admin@localhost:5432/portfolio-db')
 //TODO: Get real time prices for portfolio holdings
 router.get('/', function(req, res, next) {
   //Get all portfolio and positions from database
-  db.any("SELECT * from positions")
+  db.any("SELECT * from portfolio")
   .then(function (allPortfolioData) {
     res.json(allPortfolioData);
   })
@@ -35,21 +32,25 @@ router.get('/', function(req, res, next) {
 router.get('/:id', function(req, res, next) {
   console.log("Portfolio ID parameter is: " + req.params.id);
   let portfolioId=Number(req.params.id);
-  //portfolioObj=portfolioData[portfolioId];
-  db.manyOrNone("SELECT * from positions WHERE positions.portfolio_id=$1",portfolioId)
-  .then(function (manyOrNonePortfolioData) {
-    //IF no portfolio found then return 404
-    if (manyOrNonePortfolioData.length==0) {
-      res.status(404).send("No portfolio found for Portfolio ID: " + portfolioId);
-    }
-    else {
-      res.json(manyOrNonePortfolioData);
-    }
-  })
-  .catch(function (error) {
-    res.status(500).send(error);
-    console.log('ERROR:', error);
-  })
+  if (isNaN(portfolioId)) {
+      res.status(400).send("Portfolio ID must be a number");
+  }
+  else {
+    db.manyOrNone("SELECT * from positions WHERE positions.portfolio_id=$1",portfolioId)
+    .then(function (manyOrNonePortfolioData) {
+      //IF no portfolio found then return 404
+      if (manyOrNonePortfolioData.length==0) {
+        res.status(404).send("No portfolio found for Portfolio ID: " + portfolioId);
+      }
+      else {
+        res.json(manyOrNonePortfolioData);
+      }
+    })
+    .catch(function (error) {
+      res.status(500).send(error);
+      console.log('ERROR:', error);
+    })
+  }
 });
 
 
